a1343995419637721fa3d3745b938167f05e215c
leandrovelosos <leandrovdsantos96@gmail.com>
2025-05-03 18:48:42 -0300

    inicio pagina home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,84 @@
-import { View, Text, StyleSheet,Button } from "react-native";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, SafeAreaView } from "react-native";
+import { useState } from "react";
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-export default function Home(){
-    return(
-        <View style={styles.container}>
-            <Text>Pagina Home</Text>
-        </View>
+
+export default function Home() {
+    const [products, setProducts] = useState([
+        {
+            id: '1',
+            name: "Coca cola",
+            price: 19.90
+        },
+        {
+            id: '2',
+            name: "Chocolate",
+            price: 6.50
+        },
+        {
+            id: '4',
+            name: "Queijo 500g",
+            price: 15
+        },
+        {
+            id: '5',
+            name: "Batata frita",
+            price: 23.90
+        },
+        {
+            id: '6',
+            name: "Guarana lata",
+            price: 6.00
+        },
+    ])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.cartContent}>
+                <Text style={styles.title}>Lista de Produtos</Text>
+                <TouchableOpacity style={styles.cartButton}>
+                    <View style={styles.dot}>
+                        <Text style={styles.dotText}>3</Text>
+                    </View>
+                    <FontAwesome name="shopping-cart" size={30} color="#000" />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1
+    container: {
+        flex: 1,
+        backgroundColor: "#FAFAFA",
+        paddingEnd: 14,
+        paddingStart: 14
+    },
+    cartContent: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: 64,
+        marginBottom: 24
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold"
+    },
+    dot: {
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "red",
+        width: 20,
+        height: 20,
+        borderRadius: 12,
+        position: "absolute",
+        zIndex: 99,
+        bottom: -2,
+        left: -4
+    },
+    dotText: {
+        fontSize: 12
     }
+
 })
